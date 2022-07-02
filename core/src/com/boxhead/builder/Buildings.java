189b031950d18bdb15b3bc587f848711302f040a
CasualJavaUser <max.berezowski@gmail.com>
2022-07-02 21:26:53 +0200

    small update

--- core/src/com/boxhead/builder/Buildings.java
+++ core/src/com/boxhead/builder/Buildings.java
@@ -6,12 +6,14 @@ public class Buildings {
 
     public enum Types {
         DEFAULT_BUILDING,
-        DEFAULT_FUNCTIONAL_BUILDING
+        DEFAULT_FUNCTIONAL_BUILDING,
+        DEFAULT_RESIDENTIAL_BUILDING
     }
 
     public static Building get(Types building) {
         switch(building) {
-            case DEFAULT_FUNCTIONAL_BUILDING: return new FunctionalBuilding(new Texture("house_fungus"), Jobs.UNEMPLOYED, 1);
+            case DEFAULT_FUNCTIONAL_BUILDING: return new FunctionalBuilding(new Texture("work_fungus"), Jobs.UNEMPLOYED, 1);
+            case DEFAULT_RESIDENTIAL_BUILDING: return new ResidentialBuilding(new Texture("house_fungus"), 5);
             default: return new Building(new Texture("fungus.png"));
         }
     }
