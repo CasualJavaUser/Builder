ffd75dff35661e33439d1a93226835100da7d2a7
Chuffney <igorsocha00@gmail.com>
2022-07-03 19:19:44 +0200

    added Vector2i

--- core/src/com/boxhead/builder/NPC.java
+++ core/src/com/boxhead/builder/NPC.java
@@ -1,7 +1,5 @@
 package com.boxhead.builder;
 
-import com.sun.tools.javac.util.Pair;
-
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -12,84 +10,92 @@ public class NPC {
     ResidentialBuilding home;
 
     public static class Pathfinding {
-        private static final HashSet<Pair<Integer, Integer>> navigableTiles = new HashSet<>();
+        private static final HashSet<Vector2i> navigableTiles = new HashSet<>();
 
         public static void reset(int gridWidth, int gridHeight) {
             navigableTiles.clear();
             for (int i = 0; i < gridWidth; i++) {
                 for (int j = 0; j < gridHeight; j++) {
-                    navigableTiles.add(new Pair<>(i, j));
+                    navigableTiles.add(new Vector2i(i, j));
                 }
             }
         }
 
         public static void makeUnnavigable(int x, int y) {
-            navigableTiles.remove(new Pair<>(x, y));
+            navigableTiles.remove(new Vector2i(x, y));
+        }
+
+        public static void makeUnnavigable(Vector2i gridPosition) {
+            navigableTiles.remove(gridPosition);
         }
 
         public static void makeNavigable(int x, int y) {
-            navigableTiles.add(new Pair<>(x, y));
+            navigableTiles.add(new Vector2i(x, y));
         }
 
-        public static Pair<Integer, Integer>[] findPath(Pair<Integer, Integer> start, Pair<Integer, Integer> destination) {     //Dijkstra's algorithm
+        public static void makeNavigable(Vector2i gridPosition) {
+            navigableTiles.add(gridPosition);
+        }
+
+        public static Vector2i[] findPath(Vector2i start, Vector2i destination) {     //Dijkstra's algorithm
             if (start.hashCode() == destination.hashCode()) {
-                return new Pair[]{start};
+                return new Vector2i[]{start};
             }
-            HashSet<Pair<Integer, Integer>> unvisitedTiles = new HashSet<>(navigableTiles);
-            HashMap<Pair<Integer, Integer>, Double> distanceToTile = new HashMap<>();
-            HashMap<Pair<Integer, Integer>, Pair<Integer, Integer>> parentTree = new HashMap<>();
-            for (Pair<Integer, Integer> tile : navigableTiles) {
+            HashSet<Vector2i> unvisitedTiles = new HashSet<>(navigableTiles);
+            HashMap<Vector2i, Double> distanceToTile = new HashMap<>();
+            HashMap<Vector2i, Vector2i> parentTree = new HashMap<>();
+            for (Vector2i tile : navigableTiles) {
                 distanceToTile.put(tile, Double.MAX_VALUE);
             }
             distanceToTile.remove(start);
             distanceToTile.put(start, 0d);
 
-            int x = start.fst, y = start.snd;
-            Pair<Integer, Integer> currentTile = new Pair<>(x, y);
-            Pair<Integer, Integer> tempTile;
+            int x = start.x, y = start.y;
+            Vector2i currentTile = new Vector2i(x, y);
+            Vector2i tempTile;
 
-            while (currentTile.hashCode() != destination.hashCode()) {
-                tempTile = new Pair<>(x + 1, y);
+            while (!currentTile.equals(destination)) {
+                tempTile = new Vector2i(x + 1, y);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, 1);
-                tempTile = new Pair<>(x - 1, y);
+                tempTile = new Vector2i(x - 1, y);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, 1);
-                tempTile = new Pair<>(x, y + 1);
+                tempTile = new Vector2i(x, y + 1);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, 1);
-                tempTile = new Pair<>(x, y - 1);
+                tempTile = new Vector2i(x, y - 1);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, 1);
-                tempTile = new Pair<>(x + 1, y + 1);
+                tempTile = new Vector2i(x + 1, y + 1);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, Math.sqrt(2));
-                tempTile = new Pair<>(x - 1, y + 1);
+                tempTile = new Vector2i(x - 1, y + 1);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, Math.sqrt(2));
-                tempTile = new Pair<>(x + 1, y - 1);
+                tempTile = new Vector2i(x + 1, y - 1);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, Math.sqrt(2));
-                tempTile = new Pair<>(x - 1, y - 1);
+                tempTile = new Vector2i(x - 1, y - 1);
                 calcDistance(unvisitedTiles, distanceToTile, parentTree, currentTile, tempTile, Math.sqrt(2));
 
                 unvisitedTiles.remove(currentTile);
 
                 boolean pathExists = false;
-                for (Pair<Integer, Integer> tile : unvisitedTiles) {
+                for (Vector2i tile : unvisitedTiles) {
                     if (distanceToTile.get(tile) < Double.MAX_VALUE) {
                         pathExists = true;
                         break;
                     }
                 }
                 if (!pathExists) {
-                    return new Pair[]{start};   //no path
+                    return new Vector2i[]{start};   //no path
                 }
 
-                Pair<Integer, Integer> smallestDistanceTile = null;
+                Vector2i smallestDistanceTile = null;
                 double smallestDistance = Double.MAX_VALUE;
-                for (Pair<Integer, Integer> tile : unvisitedTiles) {
+                for (Vector2i tile : unvisitedTiles) {
                     if (distanceToTile.get(tile) < smallestDistance) {
                         smallestDistanceTile = tile;
                         smallestDistance = distanceToTile.get(tile);
                     }
                 }
                 currentTile = smallestDistanceTile;
-                x = currentTile.fst;
-                y = currentTile.snd;
+                x = currentTile.x;
+                y = currentTile.y;
             }
 
             int totalDistance = 1;
@@ -99,7 +105,7 @@ public class NPC {
             }
 
             currentTile = destination;
-            Pair<Integer, Integer>[] finalPath = new Pair[totalDistance];
+            Vector2i[] finalPath = new Vector2i[totalDistance];
             for (int i = totalDistance - 1; i >= 0; i--) {
                 finalPath[i] = currentTile;
                 currentTile = parentTree.get(currentTile);
@@ -107,11 +113,11 @@ public class NPC {
             return finalPath;
         }
 
-        private static void calcDistance(HashSet<Pair<Integer, Integer>> unvisitedTiles,
-                                         HashMap<Pair<Integer, Integer>, Double> distanceToTile,
-                                         HashMap<Pair<Integer, Integer>, Pair<Integer, Integer>> parentTree,
-                                         Pair<Integer, Integer> currentTile,
-                                         Pair<Integer, Integer> tempTile,
+        private static void calcDistance(HashSet<Vector2i> unvisitedTiles,
+                                         HashMap<Vector2i, Double> distanceToTile,
+                                         HashMap<Vector2i, Vector2i> parentTree,
+                                         Vector2i currentTile,
+                                         Vector2i tempTile,
                                          double distance) {
             if (unvisitedTiles.contains(tempTile) && distanceToTile.get(tempTile) > distanceToTile.get(currentTile) + distance) {
                 distanceToTile.remove(tempTile);
---
+++ core/src/com/boxhead/builder/Vector2i.java
@@ -0,0 +1,50 @@
+package com.boxhead.builder;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Vector2i {
+    public int x;
+    public int y;
+
+    public Vector2i() {
+        x = 0;
+        y = 0;
+    }
+
+    public Vector2i(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2i(Vector2i vector) {
+        this.x = vector.x;
+        this.y = vector.y;
+    }
+
+    public Vector2i(Vector2 vector) {
+        this.x = (int) vector.x;
+        this.y = (int) vector.y;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null) return false;
+        if (getClass() != other.getClass()) return false;
+        Vector2i vector = (Vector2i) other;
+        return x == vector.x && y == vector.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        result = 13 + Float.floatToIntBits((float) x);
+        result = 13 * result + Float.floatToIntBits((float) y);
+        return result;
+    }
+}
